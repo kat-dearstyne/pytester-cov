8587e08472d54eed92b861e2a880a06032b877e2
alexanderdamiani <alexander.damiani15@gmail.com>
2021-10-17 19:54:10 -0400

    remove docker subdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,6 @@ echo entrypoint top 2 $(ls -l)
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg python_version="$PYTHON_VERSION" -f /docker-action/Dockerfile \
-. && docker run docker-action \
+docker build -t docker-action --build-arg python_version="$PYTHON_VERSION" --file /Dockerfile-second . \
+&& docker run docker-action \
 $PYTEST_ROOT_DIR $PYTEST_TESTS_DIR $COV_OMIT_LIST $REQUIREMENTS_FILE $COV_THRESHOLD_SINGLE $COV_THRESHOLD_TOTAL
