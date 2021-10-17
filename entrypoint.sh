6cf1492895494cbadd73f0b7c6572ce264327b9d
alexanderdamiani <alexander.damiani15@gmail.com>
2021-10-17 12:58:02 -0400

    remove cd to subdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,14 @@ PYTHON_VERSION=$7
 
 echo entrypoint top 1 $(ls)
 
-cd /docker-action
+# cd /docker-action
 echo "creating docker image with python version: $PYTHON_VERSION"
 
 echo entrypoint top 2 $(ls)
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg python_version="$PYTHON_VERSION" \
+docker build -t docker-action --build-arg python_version="$PYTHON_VERSION" -f /docker-action/Dockerfile \
 . && docker run docker-action $PYTEST_ROOT_DIR $PYTEST_TESTS_DIR $COV_OMIT_LIST $REQUIREMENTS_FILE $COV_THRESHOLD_SINGLE $COV_THRESHOLD_TOTAL
 
 
