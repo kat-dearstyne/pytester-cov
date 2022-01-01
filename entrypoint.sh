02eb440f74f20fe6a0860220aec7f40a94ddcbe4
kat-dearstyne <katherine@jklfamily.com>
2022-01-01 18:49:38 -0500

    add print statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ for dir in $pytest_dirs; do
 done
 
 output=$(python3 -m pytest $pytest_cov_dirs --cov-config=.coveragerc $2)
-echo output
+echo $output
 
 # remove pytest-coverage config file
 if [ -f $cov_config_fname ]; then
